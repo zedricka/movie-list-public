14dba081bd2df1e81a709def5952fdae3702ea47
zedricka <zedrick.ara@gmail.com>
2017-12-11 09:04:36 -1000

    Updating my stuff

--- handler.js
+++ handler.js
@@ -1,6 +1,30 @@
 'use strict';
 
+// const Pool = require('pg-pool');
+// const config = require('./config.json')
+// const { table, host , database, user, password, port } = config
+// const Client = new Pool({
+//   host,
+//   database,
+//   user,
+//   password,
+//   port,
+//   idleTimeoutMillis : 1000
+// });
+//
+// let getAllMovies = "Select * FROM " + table +  " ORDER BY id ASC"
+
 module.exports.hello = (event, context, callback) => {
+  // Client.connect()
+  // .then(client => {
+  //   console.log('connected to DB ' + Client.options.database)
+  //   client.release()
+  //   return client.query(getAllMovies)
+  // })
+  // .then (res => {
+  //
+  // })
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
